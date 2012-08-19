a3fe1e5030285715f4d328ceecdaf2ea1e0eee00
Lalit Maganti <lalitmaganti@gmail.com>
2012-08-19 12:21:37 +0100

    Fix for Windows
    Exit app if wget doesn't exist

--- UltimateDownloadManager/main.cpp
+++ UltimateDownloadManager/main.cpp
@@ -1,10 +1,21 @@
 #include <QApplication>
 #include "mainwindow.h"
 #include <QColumnView>
+#include <QMessageBox>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#ifdef QT_ARCH_WINDOWS
+    if(!QFile::exists("wget.exe"))
+    {
+        QMessageBox k;
+        k.setText("Wget not found");
+        k.show();
+        return a.exec();
+    }
+#endif
     MainWindow w;
     w.showMaximized();
     return a.exec();
