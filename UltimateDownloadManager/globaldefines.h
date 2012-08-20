f96c9c8371f8262016ae703304c735b2b9d8ffa4
Lalit Maganti <lalitmaganti@gmail.com>
2012-08-20 11:10:46 +0100

    Bump version number

--- UltimateDownloadManager/globaldefines.h
+++ UltimateDownloadManager/globaldefines.h
@@ -2,7 +2,7 @@
 #define GLOBALDEFINES_H
 
 static const int majorVersion = 0;
-static const int minorVersion = 1;
+static const int minorVersion = 3;
 static const int buildVersion = 0;
 
 #endif // GLOBALDEFINES_H
